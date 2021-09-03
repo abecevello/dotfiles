dac3363d616ef130d8594c66a463969202902fb7
Adam Becevello <adam@becevello.ca>
2021-09-03 14:28:08 -0400

    Simplify install script

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
-#!/bin/sh
+#!/bin/zsh
 
-source linker.sh
+function symlink {
+  ln -nsf $1 $2
+}
+
+symlink ~/dotfiles/home/.aliases ~/.aliases
+symlink ~/dotfiles/home/.gitconfig ~/.gitconfig
+symlink ~/dotfiles/home/.shopify.gitconfig ~/.shopify.gitconfig
+symlink ~/dotfiles/home/.zshrc ~/.zshrc
