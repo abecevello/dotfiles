00df19064e5e33f42b067c1732727898d0a99e2c
Adam Becevello <adam@becevello.ca>
2021-12-02 11:39:15 -0500

    Script to use downloads directory located in iCloud drive

---
+++ icloud-drive-downloads.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cp ~/Downloads/* ~/Library/Mobile\ Documents/com~apple~CloudDocs/Downloads
+sudo rm -r ~/Downloads
+sudo ln -s ~/Library/Mobile\ Documents/com~apple~CloudDocs/Downloads ~/Downloads
