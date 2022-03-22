5ee57e008e75ef774b5546eab85b2c451156f8fc
Adam Becevello <adam@becevello.ca>
2022-03-21 20:47:03 -0400

    Use computer specific gitconfig, update install script

--- install.sh
+++ install.sh
@@ -4,7 +4,31 @@ function symlink {
   ln -nsf $1 $2
 }
 
+PS3='Computer type: '
+select computer in Personal Shopify
+do
+
+case $computer in
+  "Personal")
+  echo "Symlinking personal gitconfig"
+  symlink $PWD/home/.personal.gitconfig ~/.computer.gitconfig
+  break
+  ;;
+
+  "Shopify")
+  echo "Symlinking Shopify gitconfig"
+  symlink $PWD/home/.shopify.gitconfig ~/.computer.gitconfig
+  break
+  ;;
+
+  *)
+  echo "Invalid computer type"
+  break
+  ;;
+  esac
+done
+
+echo "Symlinking common dotfiles"
 symlink $PWD/home/.aliases ~/.aliases
-symlink $PWD/home/.gitconfig ~/.gitconfig
-symlink $PWD/home/.shopify.gitconfig ~/.shopify.gitconfig
 symlink $PWD/home/.zshrc ~/.zshrc
+symlink $PWD/home/.gitconfig ~/.gitconfig
